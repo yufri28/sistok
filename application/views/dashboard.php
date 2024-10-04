db5d564e8b758e86e14f0fdbdd1015394e185c36
Yufri Luttu <yufridonluttu@gmail.com>
2024-10-04 10:50:47 +0800

    ubah dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,7 +4,7 @@
         <div class="card w-100">
             <div class="card-body p-4">
                 <div class="table-responsive">
-                    <h2 class="text-center my-4">Sistem Managemen Stok Yayasan CBIM</h2>
+                    <h2 class="text-center my-4">Sistem Manajemen Stok Yayasan CBIM</h2>
                     <div class="d-flex justify-content-center">
                         <img style="height: auto;" src="<?=base_url('assets/images/backgrounds/bg-dashboard.jpg');?>"
                             class="img-fluid" alt="...">
